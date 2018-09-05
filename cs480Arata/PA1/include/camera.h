5962b23f36b21bc556f814845dbe7907a3c865a9
Enzo Arata <enzoarata50@nevada.unr.edu>
2018-09-05 14:34:26 -0700

    Assignment Commited

---
+++ cs480Arata/PA1/include/camera.h
@@ -0,0 +1,20 @@
+#ifndef CAMERA_H
+#define CAMERA_H
+
+#include "graphics_headers.h"
+#define GLM_ENABLE_EXPERIMENTAL
+class Camera
+{
+  public:
+    Camera();
+    ~Camera();
+    bool Initialize(int w, int h);
+    glm::mat4 GetProjection();
+    glm::mat4 GetView();
+  
+  private:
+    glm::mat4 projection;
+    glm::mat4 view;
+};
+
+#endif /* CAMERA_H */
---
+++ cs480Arata/PA1/include/object.h
@@ -0,0 +1,27 @@
+#ifndef OBJECT_H
+#define OBJECT_H
+
+#include <vector>
+#include "graphics_headers.h"
+#define GLM_ENABLE_EXPERIMENTAL
+class Object
+{
+  public:
+    Object();
+    ~Object();
+    void Update(unsigned int dt);
+    void Render();
+
+    glm::mat4 GetModel();
+
+  private:
+    glm::mat4 model;
+    std::vector<Vertex> Vertices;
+    std::vector<unsigned int> Indices;
+    GLuint VB;
+    GLuint IB;
+
+    float angle;
+};
+
+#endif /* OBJECT_H */
