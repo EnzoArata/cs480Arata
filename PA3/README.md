5e797d02b6d03975cbdb1d251a690999c088159b
Enzo Arata <35208572+EnzoArata@users.noreply.github.com>
2018-09-18 13:00:47 -0700

    Assignment #3
    weeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee

---
+++ PA3/README.md
@@ -0,0 +1,43 @@
+### Notes
+
+Sometimes when building and running my file, it will incorrectly launch. The only fix is to close the window and restart the program. Sometimes this has to be done several times, you will know its working correctly because rotational spinning of planet and moon will be good.
+
+
+A - Changes axis rotation of Planet
+Left Click- Changes rotation of Planet
+D - Changes axis rotation for Moon
+F - Changes rotation of Moon
+
+
+
+
+### CMake Instructions
+The building of the project is done using CMake, installation with apt-get or brew may be necessary. Later use with CMake and Shader files will be require the copy of a directory where those files are stored (ex. shaders). To do this in the ```add_custom_target``` function place 
+```cmake
+COMMAND ${CMAKE_COMMAND} -E copy_directory ${PROJECT_SOURCE_DIR}/shaders/ ${CMAKE_CURRENT_BINARY_DIR}/shaders
+```
+
+```bash
+mkdir build
+cd build
+cmake ..
+make
+./Tutorial
+```
+
+### Makefile Instructions 
+The makefile works as expected and must be updated with new files added in.
+
+```bash
+mkdir build
+cd build
+cp ../makefile .
+make
+./Tutorial
+```
+
+## Ubuntu.cse.unr.edu
+OpenGL 3.3 will run on the [ubuntu.cse.unr.edu](https://ubuntu.cse.unr.edu/) website. To do so follow the build instructions, but when running the Tutorial executable use this line to execute.
+```bash
+/usr/NX/scripts/vgl/vglrun ./Tutorial
+```
---
+++ PA3/src/object.cpp
@@ -0,0 +1,281 @@
+#include "object.h"
+#include "graphics.h"
+#include <vector>
+#define GLM_ENABLE_EXPERIMENTAL
+
+Object *objectReference;
+
+Object::Object(Engine *engineReference)
+{
+    engineRef = engineReference;
+    parentRef = NULL;
+  /*
+    # Blender File for a Cube
+    o Cube
+    v 1.000000 -1.000000 -1.000000
+    v 1.000000 -1.000000 1.000000
+    v -1.000000 -1.000000 1.000000
+    v -1.000000 -1.000000 -1.000000
+    v 1.000000 1.000000 -0.999999
+    v 0.999999 1.000000 1.000001
+    v -1.000000 1.000000 1.000000
+    v -1.000000 1.000000 -1.000000
+    s off
+    f 2 3 4
+    f 8 7 6
+    f 1 5 6
+    f 2 6 7
+    f 7 8 4
+    f 1 4 8
+    f 1 2 4
+    f 5 8 6
+    f 2 1 6
+    f 3 2 7
+    f 3 7 4
+    f 5 1 8
+  */
+
+  Vertices = {
+    {{1.0f, -1.0f, -1.0f}, {0.0f, 0.0f, 0.0f}},
+    {{1.0f, -1.0f, 1.0f}, {1.0f, 0.0f, 0.0f}},
+    {{-1.0f, -1.0f, 1.0f}, {0.0f, 1.0f, 0.0f}},
+    {{-1.0f, -1.0f, -1.0f}, {0.0f, 0.0f, 1.0f}},
+    {{1.0f, 1.0f, -1.0f}, {1.0f, 1.0f, 0.0f}},
+    {{1.0f, 1.0f, 1.0f}, {1.0f, 0.0f, 1.0f}},
+    {{-1.0f, 1.0f, 1.0f}, {0.0f, 1.0f, 1.0f}},
+    {{-1.0f, 1.0f, -1.0f}, {1.0f, 1.0f, 1.0f}}
+  };
+
+  Indices = {
+    2, 3, 4,
+    8, 7, 6,
+    1, 5, 6,
+    2, 6, 7,
+    7, 8, 4,
+    1, 4, 8,
+    1, 2, 4,
+    5, 8, 6,
+    2, 1, 6,
+    3, 2, 7,
+    3, 7, 4,
+    5, 1, 8
+  };
+
+  // The index works at a 0th index
+  for(unsigned int i = 0; i < Indices.size(); i++)
+  {
+    Indices[i] = Indices[i] - 1;
+  }
+
+  angle = 0.0f;
+
+  glGenBuffers(1, &VB);
+  glBindBuffer(GL_ARRAY_BUFFER, VB);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(Vertex) * Vertices.size(), &Vertices[0], GL_STATIC_DRAW);
+
+  glGenBuffers(1, &IB);
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, IB);
+  glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(unsigned int) * Indices.size(), &Indices[0], GL_STATIC_DRAW);
+
+  rotationRate = 1.0f;
+  rotationRate2 = 1.0f;
+  angleMultiplier = 1.0f;
+  angle2Multiplier = -1.0f;
+  scaleValue = glm::vec3(1.0, 1.0, 1.0);
+}
+
+Object::Object(Engine *engineReference, Object *parentReference)
+{
+    engineRef = engineReference;
+  /*
+    # Blender File for a Cube
+    o Cube
+    v 1.000000 -1.000000 -1.000000
+    v 1.000000 -1.000000 1.000000
+    v -1.000000 -1.000000 1.000000
+    v -1.000000 -1.000000 -1.000000
+    v 1.000000 1.000000 -0.999999
+    v 0.999999 1.000000 1.000001
+    v -1.000000 1.000000 1.000000
+    v -1.000000 1.000000 -1.000000
+    s off
+    f 2 3 4
+    f 8 7 6
+    f 1 5 6
+    f 2 6 7
+    f 7 8 4
+    f 1 4 8
+    f 1 2 4
+    f 5 8 6
+    f 2 1 6
+    f 3 2 7
+    f 3 7 4
+    f 5 1 8
+  */
+
+  Vertices = {
+    {{1.0f, -1.0f, -1.0f}, {0.0f, 0.0f, 0.0f}},
+    {{1.0f, -1.0f, 1.0f}, {1.0f, 0.0f, 0.0f}},
+    {{-1.0f, -1.0f, 1.0f}, {0.0f, 1.0f, 0.0f}},
+    {{-1.0f, -1.0f, -1.0f}, {0.0f, 0.0f, 1.0f}},
+    {{1.0f, 1.0f, -1.0f}, {1.0f, 1.0f, 0.0f}},
+    {{1.0f, 1.0f, 1.0f}, {1.0f, 0.0f, 1.0f}},
+    {{-1.0f, 1.0f, 1.0f}, {0.0f, 1.0f, 1.0f}},
+    {{-1.0f, 1.0f, -1.0f}, {1.0f, 1.0f, 1.0f}}
+  };
+
+  Indices = {
+    2, 3, 4,
+    8, 7, 6,
+    1, 5, 6,
+    2, 6, 7,
+    7, 8, 4,
+    1, 4, 8,
+    1, 2, 4,
+    5, 8, 6,
+    2, 1, 6,
+    3, 2, 7,
+    3, 7, 4,
+    5, 1, 8
+  };
+
+  // The index works at a 0th index
+  for(unsigned int i = 0; i < Indices.size(); i++)
+  {
+    Indices[i] = Indices[i] - 1;
+  }
+
+  angle = 0.0f;
+
+  glGenBuffers(1, &VB);
+  glBindBuffer(GL_ARRAY_BUFFER, VB);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(Vertex) * Vertices.size(), &Vertices[0], GL_STATIC_DRAW);
+
+  glGenBuffers(1, &IB);
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, IB);
+  glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(unsigned int) * Indices.size(), &Indices[0], GL_STATIC_DRAW);
+
+  rotationRate = 1.0f;
+  rotationRate2 = 1.0f;
+  scaleValue = glm::vec3(1.0, 1.0, 1.0);
+  angleMultiplier = 1.0f;
+  angle2Multiplier = -1.0f;
+
+  parentRef = parentReference;
+  parentRef->addChild(this);
+}
+
+Object::~Object()
+{
+  Vertices.clear();
+  Indices.clear();
+}
+
+void Object::Update(unsigned int dt, glm::mat4 parentMatrix)
+{
+    if(parentRef == NULL)
+    {
+        if(engineRef->rotationDirection == false)
+        {
+            angleMultiplier = -1.0;
+        }
+        if(engineRef->rotationDirection == true)
+        {
+            angleMultiplier = 1.0;
+        }
+        if(engineRef->rotationMovement == false)
+        {
+            angle2Multiplier =  -1.0;
+        }
+        if(engineRef->rotationMovement == true)
+        {
+            angle2Multiplier = 1.0;
+        }
+    }
+    else
+    {
+        if(engineRef->rotationDirection2 == false)
+        {
+            angleMultiplier = -1.0;
+        }
+        if(engineRef->rotationDirection2 == true)
+        {
+            angleMultiplier = 1.0;
+        }
+        if(engineRef->rotationMovement2 == false)
+        {
+            angle2Multiplier =  -1.0;
+        }
+        if(engineRef->rotationMovement2 == true)
+        {
+            angle2Multiplier = 1.0;
+        }
+    }
+
+
+
+    angle += angleMultiplier * rotationRate * dt * M_PI/1000;
+    angle2 += angle2Multiplier * rotationRate2 * dt * M_PI/1000;
+
+
+    model =parentMatrix * glm::translate( glm::mat4(1.0f), glm::vec3( 3.0f * cos( angle2 ), 0.0f, 3.0f* sin( angle2) ) );
+
+    glm::mat4 modelRef = model;
+
+    for(int i=0; i<childrenRef.size();i++)
+    {
+
+        childrenRef[i]->Update(dt, modelRef);
+
+    }
+
+    model *=   glm::rotate( model, (angle), glm::vec3(0.0, 1.0, 0.0))
+             * glm::scale( scaleValue );
+
+
+
+
+
+
+
+
+
+
+}
+
+glm::mat4 Object::GetModel()
+{
+  return model;
+}
+
+void Object::Render()
+{
+  glEnableVertexAttribArray(0);
+  glEnableVertexAttribArray(1);
+
+  glBindBuffer(GL_ARRAY_BUFFER, VB);
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), 0);
+  glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)offsetof(Vertex,color));
+
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, IB);
+
+  glDrawElements(GL_TRIANGLES, Indices.size(), GL_UNSIGNED_INT, 0);
+
+  glDisableVertexAttribArray(0);
+  glDisableVertexAttribArray(1);
+}
+
+
+void Object::addChild(Object *childReference)
+{
+
+    childrenRef.push_back (childReference);
+
+}
+
+void Object::setScale(glm::vec3 newSize)
+{
+
+    scaleValue = newSize;
+
+}
